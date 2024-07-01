b8596284b55f305e301dc31c5611ee29010faee0
Vaibhav Baneshi <vaibhavbaneshi@.com>
2024-07-01 16:56:43 +0530

    Added Navbar Component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-logo">
+          <span className='app-header'>Movie Watchlist</span>
+        </Link>
+      </div>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/watchlist" className="nav-link">
+            Watchlist
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/explore" className="nav-link">
+            Explore Movies
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
